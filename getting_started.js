6697636f22b5c47037512ea18dc06dca007e0b33
Adam Caron <adamwcaron@gmail.com>
2015-10-10 17:21:04 -0600

    Remove comments

--- getting_started.js
+++ getting_started.js
@@ -6,16 +6,3 @@ var worker = function(collection) {
 };
 
 module.exports = worker
-
-// ## Your Mission
-
-// We have a array of Javascript Objects representing some users in the following format:
-
-//     [
-//         { id: 22, username: "martin", active: true},
-//         { id: 23, username: "max",    active: false},
-//         { id: 24, username: "linda",  active: false}
-//     ]
-
-// Please write a function that takes such a list as its first argument and give back only
-// the active users (active === true).
